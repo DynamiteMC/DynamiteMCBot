91c96d61f9c8ad15bf38fe7449c8856db43aa336
oq <xcheez4@gmail.com>
2023-09-07 15:49:52 +0300

    add -u to fetchdb

--- commands/fetchdb.go
+++ commands/fetchdb.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"gobot/store"
 	"strings"
 
@@ -15,21 +16,50 @@ var Command_fetchdb = Command{
 	Aliases:     []string{},
 	Execute: func(message *events.MessageCreate, args []string) {
 		db := GetArgument(args, 0)
-		var ids []string
 		if db == "" {
 			return
 		}
-		switch db {
-		case "corner":
-			for i := range store.GetCorners() {
-				ids = append(ids, i)
+		if db == "-u" {
+			db = GetArgument(args, 1)
+			if db == "" {
+				return
 			}
-		case "muted":
-			for i := range store.GetMuted() {
-				ids = append(ids, i)
+			user := GetArgument(args, 2)
+			if user == "" {
+				return
 			}
+			id := ParseMention(user)
+			if id == 0 {
+				CreateMessage(message, Message{Content: "Failed to parse member", Reply: true})
+				return
+			}
+			switch db {
+			case "muted":
+				CreateMessage(message, fmt.Sprint(store.IsMuted(int64(id))), true)
+			case "corner":
+				{
+					is, corner := store.GetCorner(int64(id))
+					if !is {
+						CreateMessage(message, "false", true)
+						return
+					}
+					CreateMessage(message, fmt.Sprint(corner), true)
+				}
+			}
+		} else {
+			var ids []string
+			switch db {
+			case "corner":
+				for i := range store.GetCorners() {
+					ids = append(ids, i)
+				}
+			case "muted":
+				for i := range store.GetMuted() {
+					ids = append(ids, i)
+				}
+			}
+			embed := discord.NewEmbedBuilder().SetTitlef("%d entries", len(ids)).SetDescription(strings.Join(ids, "\n")).SetColor(color)
+			CreateMessage(message, embed.Build(), true)
 		}
-		embed := discord.NewEmbedBuilder().SetTitlef("%d entries", len(ids)).SetDescription(strings.Join(ids, "\n")).SetColor(color)
-		CreateMessage(message, embed.Build(), true)
 	},
 }
